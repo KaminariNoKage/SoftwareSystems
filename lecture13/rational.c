5b0c4cea00be4a80633f0aa0e5c498bbad53aa78
Your Name <kaiblogs@hotmail.com>
2014-03-10 11:24:51 -0400

    Quiz

--- lecture13/rational.c
+++ lecture13/rational.c
@@ -12,19 +12,21 @@ typedef struct {
 // Returns a new Rational with the given numer and denom.
 // If unable to allocate, prints an error message and exits.
 Rational *make_rational(int numer, int denom) {
-    // FILL THIS IN
-    return NULL;
+    Rational *ration = malloc(sizeof(Rational));
+    ration->numer = numer;
+    ration->denom = denom;
+    return ration;
 }
 
 // Computes a floating-point approximation of a Rational.
 double rational_to_double(Rational *rational) {
-    // FILL THIS IN
-    return 0.0;
+    double nuNum = rational->numer/rational->denom;
+    return nuNum;
 }
 
 // Multiplies two rational numbers; returns a new Rational.
 Rational *mult_rational(Rational *r1, Rational *r2) {
-    // FILL THIS IN
+    Rational *nuRat = malloc(sizeof(Rational));
     return NULL;
 }
 
