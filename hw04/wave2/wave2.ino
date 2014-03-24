3f1fc3a332740baa09ebddf18b43faf8ac0185b8
KaminariNoKage <kaiblogs@hotmail.com>
2014-03-23 23:21:50 -0400

    Updated synth test

--- hw04/wave2/wave2.ino
+++ hw04/wave2/wave2.ino
@@ -35,13 +35,44 @@ void setup() {
   pinMode(6, OUTPUT);  
 }
 
+int reverseBits(int num)
+{
+    int count = sizeof(num) * 8 - 1;
+    int reverse_num = num;
+    
+    num >>= 1; 
+    while(num)
+    {
+       reverse_num <<= 1;       
+       reverse_num |= num & 1;
+       num >>= 1;
+       count--;
+    }
+    reverse_num <<= count;
+    return reverse_num;
+}
+
 void writeByte(int x) {
   int pin;
   
-  for (pin=13; pin>=6; pin--) {
-    digitalWrite(pin, x&1);
-    x >>= 1;
-  }
+  //for (pin=13; pin>=6; pin--) {
+   // digitalWrite(pin, x&1);
+   // x >>= 1;
+  //}
+  
+  // set pins 1 (serial transmit) and 2..7 as output,
+  // but leave pin 0 (serial receive) as input
+  // (otherwise serial port will stop working!) ...
+  DDRD = reverseBits(B11111110);  // digital pins 7,6,5,4,3,2,1,0
+
+  // set pins 8..13 as output...
+  DDRB = reverseBits(B00111111);  // digital pins -,-,13,12,11,10,9,8
+
+  // Turn off digital output pins 2..7 ...
+  PORTD &= B00000011;   // turns off 2..7, but leaves pins 0 and 1 alone
+
+  // Write simultaneously to pins 8..13...
+  PORTB = B00111000;   // turns on 13,12,11; turns off 10,9,8
 }
 
 int low = 36;
